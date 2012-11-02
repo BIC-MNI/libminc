d09f4d467819dea07b61f7dbcd6c15ee8bf37520
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-11-02 16:59:42 -0400

    restored missing file

---
+++ config.h.cmake
@@ -0,0 +1,52 @@
+/* various defines */
+
+#define MINC2 @MINC2@
+#define PACKAGE_NAME "@PACKAGE_NAME@"
+#define PACKAGE_BUGREPORT "@PACKAGE_BUGREPORT@"
+#define PACKAGE_VERSION "@PACKAGE_VERSION@"
+#define PACKAGE_STRING "@PACKAGE_STRING@"
+#define VERSION PACKAGE_VERSION
+
+#define H5Acreate_vers 2
+
+#cmakedefine HAVE_MKSTEMP 1 
+#cmakedefine HAVE_STRERROR 1 
+#cmakedefine HAVE_FLOAT_H 1 
+
+#cmakedefine HAVE_DIRENT_H 1 
+#cmakedefine HAVE_DLFCN_H 1 
+#cmakedefine HAVE_FCNTL_H 1 
+#cmakedefine HAVE_FORK 1 
+#cmakedefine HAVE_GETPWNAM 1 
+#cmakedefine HAVE_INT16_T 1 
+#cmakedefine HAVE_INT32_T 1 
+#cmakedefine HAVE_INTTYPES_H 1 
+#cmakedefine HAVE_MEMORY_H 1 
+#cmakedefine HAVE_MKSTEMP 1 
+#cmakedefine HAVE_NDIR_H 1 
+#cmakedefine HAVE_POPEN 1 
+#cmakedefine HAVE_PWD_H 1 
+#cmakedefine HAVE_SELECT 1 
+#cmakedefine HAVE_STDINT_H 1 
+#cmakedefine HAVE_STDLIB_H 1 
+#cmakedefine HAVE_STRDUP 1 
+#cmakedefine HAVE_SYSCONF 1 
+#cmakedefine HAVE_SYSTEM 1 
+#cmakedefine HAVE_SYS_DIR_H 1 
+#cmakedefine HAVE_SYS_NDIR_H 1 
+#cmakedefine HAVE_SYS_STAT_H 1 
+#cmakedefine HAVE_SYS_TIME_H 1 
+#cmakedefine HAVE_SYS_TYPES_H 1 
+#cmakedefine HAVE_SYS_WAIT_H 1 
+#cmakedefine HAVE_TEMPNAM 1 
+#cmakedefine HAVE_TMPNAM 1 
+#cmakedefine HAVE_UNISTD_H 1 
+#cmakedefine HAVE_VALUES_H 1 
+#cmakedefine HAVE_VFORK 1 
+#cmakedefine HAVE_VFORK_H 1 
+#cmakedefine HAVE_WORKING_FORK 1 
+#cmakedefine HAVE_WORKING_VFORK 1 
+#cmakedefine HAVE_ZLIB 1 
+#cmakedefine HAVE_STRINGS_H 1 
+#cmakedefine HAVE_STRING_H 1 
+
