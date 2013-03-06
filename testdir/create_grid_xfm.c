5004fc285ee06a4c36200ce0460982430318e8e2
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-03-06 17:31:36 -0500

    Adjusting for new interface in libminc

--- testdir/create_grid_xfm.c
+++ testdir/create_grid_xfm.c
@@ -25,16 +25,16 @@ int main( int ac, char* av[] )
     set_minc_input_vector_to_scalar_flag( &mio, 0 );
 
     st = input_volume( av[1],
-		       0, NULL, 
-		       MI_ORIGINAL_TYPE, FALSE, 0.0, 0.0, 
+		       0, NULL,
+		       MI_ORIGINAL_TYPE, FALSE, 0.0, 0.0,
 		       TRUE, &v,
 		       &mio );
-    if ( st != OK ) { 
+    if ( st != OK ) {
 	fprintf( stderr, "failed to read grid volume \"%s\"\n", av[1] );
 	return 1;
     }
 
-    create_grid_transform( &t, v );
+    create_grid_transform( &t, v, NULL );
     st = output_transform_file( av[2], time_stamp(ac,av), &t );
 
     if ( st != OK ) {
