cf1ac730143ae1f5fef12d8c40db57f0579444a7
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2013-11-27 22:32:13 -0500

    Forgot to add new files

---
+++ libsrc/minc_format_convert.h
@@ -0,0 +1,22 @@
+#ifndef __MINC_FORMAT_CONVERT_H__
+#define __MINC_FORMAT_CONVERT_H__
+
+
+/* ----------------------------- MNI Header -----------------------------------
+@NAME       : minc_format_convert.h
+@COPYRIGHT  : Copyright 2013 Vladimir S. FONOV , McConnell Brain Imaging Centre,
+              Copyright 1993 Peter Neelin, McConnell Brain Imaging Centre, 
+              Montreal Neurological Institute, McGill University.
+              Permission to use, copy, modify, and distribute this
+              software and its documentation for any purpose and without
+              fee is hereby granted, provided that the above copyright
+              notice appear in all copies.  The author and McGill University
+              make no representations about the suitability of this
+              software for any purpose.  It is provided "as is" without
+              express or implied warranty.
+@RCSID      : $Header: /private-cvsroot/minc/libsrc/minc.h,v 6.21 2010-05-19 03:44:25 stever Exp $ MINC (MNI)
+---------------------------------------------------------------------------- */
+
+
+
+#endif /*__MINC_FORMAT_CONVERT_H__*/
