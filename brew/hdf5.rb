f134ca9ca398ef64d82bafc70825eac188377710
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2016-10-17 16:03:17 -0400

    Updated brew control sum for HDF5

--- brew/hdf5.rb
+++ brew/hdf5.rb
@@ -1,7 +1,7 @@
 class Hdf5 < Formula
   homepage "https://www.hdfgroup.org/HDF5/"
   url "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15-patch1/src/hdf5-1.8.15-patch1.tar.bz2"
-  md5 "3c0d7a8c38d1abc7b40fc12c1d5f2bb8"
+  sha256 "a5afc630c4443547fff15e9637b5b10404adbed4c00206d89517d32d6668fb32"
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
