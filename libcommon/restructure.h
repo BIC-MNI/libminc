b1cd7f5036a7ceaf6d15fc76d50840e2b6c20730
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2016-10-19 11:33:31 -0400

    Updated comment for restructure

--- libcommon/restructure.h
+++ libcommon/restructure.h
@@ -2,10 +2,17 @@
  * \file restructure.h
  * \brief Just declares the prototype of restructure_array().
  */
-extern void restructure_array(size_t ndims,
-                              unsigned char *array, 
-                              const size_t *lengths_perm,
-                              size_t el_size,
-                              const int *map,
-                              const int *dir);
+#ifndef MINC_RESTRUCTURE_H
+#define MINC_RESTRUCTURE_H
 
+/** Reorganize data in a multidimensional array "in place".
+ *  Uses temporary buffer of nelem/8 bytes
+ */
+void restructure_array(size_t ndims,
+                      unsigned char *array, 
+                      const size_t *lengths_perm,
+                      size_t el_size,
+                      const int *map,
+                      const int *dir);
+
+#endif /*MINC_RESTRUCTURE_H*/
