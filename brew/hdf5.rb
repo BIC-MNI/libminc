78d518215db329ddd3bce126c0cd72ae567956c7
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2017-09-11 00:22:15 -0400

    Fixed hdf5 url for travis ci build

--- brew/hdf5.rb
+++ brew/hdf5.rb
@@ -1,7 +1,7 @@
 class Hdf5 < Formula
   homepage "https://www.hdfgroup.org/HDF5/"
-  url "https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.15-patch1/src/hdf5-1.8.15-patch1.tar.bz2"
-  sha256 "a5afc630c4443547fff15e9637b5b10404adbed4c00206d89517d32d6668fb32"
+  url "https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8/hdf5-1.8.19/src/hdf5-1.8.19.tar.bz2"
+  sha256 "8755d7290c4274de5e897840b2bcd15cffe7b5cbcac4a7bc0c430b83d7f0944b"
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make", "install"
