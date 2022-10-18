5b068486b306f3906d51d9fe0522015f5d9281cc
Sean McBride <sean@rogue-research.com>
2022-10-18 16:07:40 -0400

    Updated several URLs from http to https
    I checked that each of these http links are currently redirecting to https anyway.
    
    For the mailman list, also updated the URL to where the previous URL is currently redirecting to.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -7,7 +7,7 @@
 set(CTEST_PROJECT_NAME "libminc")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=libminc")
 set(CTEST_DROP_SITE_CDASH TRUE)
--- brew/netcdf.rb
+++ brew/netcdf.rb
@@ -1,5 +1,5 @@
 class Netcdf < Formula
-  homepage "http://www.unidata.ucar.edu/software/netcdf/"
+  homepage "https://www.unidata.ucar.edu/software/netcdf/"
   url "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.3.1.tar.gz"
   sha256 "bdde3d8b0e48eed2948ead65f82c5cfb7590313bc32c4cf6c6546e4cea47ba19"
   def install
