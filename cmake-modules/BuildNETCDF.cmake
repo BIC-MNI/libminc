df4d7a81266260b5bed835211a2db678d48784c4
Hans J. Johnson <hans-johnson@uiowa.edu>
2026-02-09 14:14:54 -0600

    STYLE: Remove end of line spacing from cmake files
    Followup on previous removal of trailing white space
    in code files.

--- cmake-modules/BuildNETCDF.cmake
+++ cmake-modules/BuildNETCDF.cmake
@@ -1,14 +1,14 @@
 
 macro(build_netcdf install_prefix)
 
-ExternalProject_Add(NETCDF 
+ExternalProject_Add(NETCDF
   SOURCE_DIR NETCDF
   URL "ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.0.1.tar.gz"
   URL_MD5 "a251453c5477599f050fa4e593295186"
   BUILD_IN_SOURCE 1
   INSTALL_DIR "${install_prefix}"
-  BUILD_COMMAND   make 
-  INSTALL_COMMAND make install 
+  BUILD_COMMAND   make
+  INSTALL_COMMAND make install
   CONFIGURE_COMMAND ./configure --prefix=${install_prefix} --with-pic --disable-netcdf4 --disable-hdf4 --disable-dap --disable-shared --disable-cxx --disable-f77 --disable-f90 --disable-examples --enable-v2 --disable-docs
 )
 
